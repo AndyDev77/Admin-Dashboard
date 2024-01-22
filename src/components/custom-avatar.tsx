2bf48ba649aa1ce6890df521e297dbddfef499e5
AndyDev77 <andyvespuce77550@gmail.com>
2024-01-23 00:36:47 +0100

    create deals-chart

--- src/components/custom-avatar.tsx
+++ src/components/custom-avatar.tsx
@@ -1,9 +1,25 @@
-import {Avatar as AntdAvatar} from "antd"
+import { getNameInitials } from '@/utilities';
+import { Avatar as AntdAvatar, AvatarProps } from 'antd'
 
-const CustomAvatar = () => {
+type Props = AvatarProps & {
+  name?: string;
+}
+
+const CustomAvatar = ({ name, style, ...rest }: Props) => {
   return (
-    <AntdAvatar alt={"Andy Vespuce"} size="small">
-        AV
+    <AntdAvatar
+      alt={name}
+      size="small"
+      style={{ 
+        backgroundColor: '#87d068',
+        display: 'flex',
+        alignItems: 'center',
+        border: 'none',
+        ...style
+       }}
+      {...rest}
+    >
+      {getNameInitials(name || '')}
     </AntdAvatar>
   )
 }
