707d1d7b9e3e07bd20df46f1d93ae643141b5da3
AndyDev77 <andyvespuce77550@gmail.com>
2024-01-24 14:54:34 +0100

    final admin-dashboard

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,32 @@
-import { Authenticated, GitHubBanner, Refine } from "@refinedev/core";
-
+import {
+  Authenticated,
+  GitHubBanner,
+  Refine,
+  WelcomePage,
+} from "@refinedev/core";
 import { DevtoolsPanel, DevtoolsProvider } from "@refinedev/devtools";
-
 import { RefineKbar, RefineKbarProvider } from "@refinedev/kbar";
 
 import { useNotificationProvider } from "@refinedev/antd";
 import "@refinedev/antd/dist/reset.css";
 
 import { authProvider, dataProvider, liveProvider } from "./providers";
-import { Home, ForgotPassword, Login, Register } from "./pages";
+import { Home, ForgotPassword, Login, Register, CompanyList } from "./pages";
 
 import routerBindings, {
   CatchAllNavigate,
   DocumentTitleHandler,
   UnsavedChangesNotifier,
 } from "@refinedev/react-router-v6";
-
 import { App as AntdApp } from "antd";
-
 import { BrowserRouter, Outlet, Route, Routes } from "react-router-dom";
-
 import Layout from "./components/layout";
 import { resources } from "./config/resources";
+import Create from "./pages/company/create";
+import Edit from "./pages/company/edit";
+import List from "./pages/tasks/list";
+import EditTask from "./pages/tasks/edit";
+import CreateTask from "./pages/tasks/create";
 
 function App() {
   return (
@@ -62,6 +67,22 @@ function App() {
                   }
                 >
                   <Route index element={<Home />} />
+                  <Route path="/companies">
+                    <Route index element={<CompanyList />} />
+                    <Route path="new" element={<Create />} />
+                    <Route path="edit/:id" element={<Edit />} />
+                  </Route>
+                  <Route
+                    path="/tasks"
+                    element={
+                      <List>
+                        <Outlet />
+                      </List>
+                    }
+                  >
+                    <Route path="new" element={<CreateTask />} />
+                    <Route path="edit/:id" element={<EditTask />} />
+                  </Route>
                 </Route>
               </Routes>
               <RefineKbar />
--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,28 +1,74 @@
+import LatestActivities from "@/components/home/latest-activities"
+import DashboardTotalCountCard from "@/components/home/total-count-card"
 import { DealsChart, UpcomingEvents } from "@/components/index"
+import { DASHBOARD_TOTAL_COUNTS_QUERY } from "@/graphql/queries"
+import { DashboardTotalCountsQuery } from "@/graphql/types"
+import { useCustom } from "@refinedev/core"
 import { Col, Row } from "antd"
 
 export const Home = () => {
+  const { data, isLoading } = useCustom<DashboardTotalCountsQuery>({
+    url: '',
+    method: 'get',
+    meta: {
+      gqlQuery: DASHBOARD_TOTAL_COUNTS_QUERY
+    }
+  }) 
+
   return (
-    <Row 
+    <div>
+      <Row gutter={[32, 32]}>
+        <Col xs={24} sm={24} xl={8}>
+          <DashboardTotalCountCard 
+            resource="companies"
+            isLoading={isLoading}
+            totalCount={data?.data.companies.totalCount}
+          />
+        </Col>
+        <Col xs={24} sm={24} xl={8}>
+          <DashboardTotalCountCard 
+            resource="contacts"
+            isLoading={isLoading}
+            totalCount={data?.data.contacts.totalCount}
+          />
+        </Col>
+        <Col xs={24} sm={24} xl={8}>
+          <DashboardTotalCountCard 
+            resource="deals"
+            isLoading={isLoading}
+            totalCount={data?.data.deals.totalCount}
+          />
+        </Col>
+      </Row>
+
+
+      <Row 
       gutter={[32, 32]} 
       style={{marginTop: "32px"}}
-    >
-      <Col 
+      >
+        <Col 
         xs={24} 
         sm={24} 
         xl={8} 
         style={{height: "460px"}}
-      >
-        <UpcomingEvents />
-      </Col>
-      <Col 
+        >
+          <UpcomingEvents />
+        </Col>
+        <Col 
         xs={24} 
         sm={24} 
-        xl={15} 
+        xl={16} 
         style={{height: "460px"}}
-      >
-        <DealsChart />
-      </Col>
-    </Row>
+        >
+          <DealsChart />
+        </Col>
+      </Row>
+
+      <Row gutter={[32, 32]} style={{marginTop: "32px"}}>
+        <Col xs={24}>
+          <LatestActivities />
+        </Col>
+      </Row>
+    </div>
   )
 }
